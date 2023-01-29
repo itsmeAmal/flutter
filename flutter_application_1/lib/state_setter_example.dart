c2122687b46b7f0d44ed80b92620e46fbf3a53ea
amal wickramarathna <wickramarathna.amal@gmail.com>
2023-01-29 10:17:56 +0530

    State Setter

---
+++ flutter_application_1/lib/state_setter_example.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class stateSetterExample extends StatefulWidget {
+  const stateSetterExample({super.key});
+
+  @override
+  State<stateSetterExample> createState() => _stateSetterExampleState();
+}
+
+class _stateSetterExampleState extends State<stateSetterExample> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
